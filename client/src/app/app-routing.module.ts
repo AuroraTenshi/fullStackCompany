2994742dc19de4da5b2649e062ab98a86d44315f
unknown <takacslaczy@gmail.com>
2020-12-02 15:42:08 +0100

    Client: Menu added. It is basic
    This is the begining of the menu. The button doesn't function yet.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'menu'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
